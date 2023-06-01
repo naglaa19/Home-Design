f031f599f5256b57bd506a1d64abf6135c447101
naglaa19 <naglaaassem43@gmail.com>
2023-06-01 23:33:47 +0300

    vr page

--- public/virtual_js/main.js
+++ public/virtual_js/main.js
@@ -1,5 +1,5 @@
 /*
-	Directive by HTML5 UP
+	Telephasic by HTML5 UP
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
@@ -11,12 +11,11 @@
 
 	// Breakpoints.
 		breakpoints({
-			wide:      [ '1281px',  '1680px' ],
-			normal:    [ '981px',   '1280px' ],
-			narrow:    [ '841px',   '980px'  ],
-			narrower:  [ '737px',   '840px'  ],
-			mobile:    [ '481px',   '736px'  ],
-			mobilep:   [ null,      '480px'  ]
+			normal:    [ '1081px',  '1280px'  ],
+			narrow:    [ '821px',   '1080px'  ],
+			narrower:  [ '737px',   '820px'   ],
+			mobile:    [ '481px',   '736px'   ],
+			mobilep:   [ null,      '480px'   ]
 		});
 
 	// Play initial animations on page load.
@@ -25,5 +24,43 @@
 				$body.removeClass('is-preload');
 			}, 100);
 		});
+
+	// Dropdowns.
+		$('#nav > ul').dropotron({
+			mode: 'fade',
+			speed: 300,
+			alignment: 'center',
+			noOpenerFade: true
+		});
+
+	// Nav.
+
+		// Button.
+			$(
+				'<div id="navButton">' +
+					'<a href="#navPanel" class="toggle"></a>' +
+				'</div>'
+			)
+				.appendTo($body);
+
+		// Panel.
+			$(
+				'<div id="navPanel">' +
+					'<nav>' +
+						'<a href="index.html" class="link depth-0">Home</a>' +
+						$('#nav').navList() +
+					'</nav>' +
+				'</div>'
+			)
+				.appendTo($body)
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'top',
+					target: $body,
+					visibleClass: 'navPanel-visible'
+				});
 
 })(jQuery);
--- resources/views/user/virtual.blade.php
+++ resources/views/user/virtual.blade.php
@@ -10,64 +10,68 @@
         <!-- Styles -->
         <link rel="stylesheet" type="text/css" href="{{asset('css/virtual.css')}}">
     </head>
-    <body class="is-preload">
+    <body class="homepage is-preload">
+		<div id="page-wrapper">
 
-		<!-- Header -->
-			<div id="header">
-				<h1>Virtual Reality Showroom</h1>
-				<p>Virtual Reality (VR) is an innovative technology that allows users to experience <br>three-dimensional computer-generated environments in a fully immersive way.
-			</div>
+			<!-- Header -->
+				<div id="header-wrapper">
+					
 
-		<!-- Main -->
-			<div id="main">
+					<!-- Hero -->
+						<section id="hero" class="container">
+							<header>
+								<h2>Virtual Reality Showroom</h2>
+							</header>
+							<p>Virtual Reality (VR) is an innovative technology that allows users to experience three-dimensional computer-generated environments in a fully immersive way.</p>
+						</section>
 
-				<header class="major container medium">
-					<h2>Why VR used in home design?</h2>
-				</header>
-
-				<div class="box alt container">
-					<section class="feature left">
-						<a href="#" class="image icon solid "><img src="{{asset('images/virtual/ikea-room-plan.jpg')}}" alt="" /></a>
-						<div class="content">
-							<h3>Importing the model into VR</h3>
-							<p>A designer or architect creates a 3D model of the home and The 3D model is then imported into virtual reality software, which allows the user to explore and interact with the model in a 3D environment.</p>
-						</div>
-					</section>
-
-
-					<section class="feature right">
-						<a href="#" class="image icon solid "><img src="{{asset('images/virtual/VR design.jpg')}}" alt="" /></a>
-						<div class="content">
-							<h3>Exploring the model</h3>
-							<p> The user can walk through the virtual home, examining details such as the layout, materials, and finishes. They can also interact with objects, such as opening doors and drawers, adjusting lighting, and moving furniture.</p>
-						</div>
-					</section>
-
-
-					<section class="feature left">
-						<a href="#" class="image icon solid "><img src="{{asset('images/virtual/VR-Placing-Furniture.jpg')}}" alt="" /></a>
-						<div class="content">
-							<h3>Making changes</h3>
-							<p>Based on their experience in VR, the user can make changes to the design, such as adjusting the layout or changing materials. The designer or architect can then update the 3D model to reflect these changes, and the process can be repeated until the user is satisfied with the design.</p>
-						</div>
-					</section>
 				</div>
+			
+    <div class="wrapper">
+        <section class="container">
+            <header class="major">
+                <h2>Why VR used in home design?</h2>
+                
+            </header>
+            <div class="row features">
+                <section class="col-4 col-12-narrower feature">
+                    <div class="image-wrapper first">
+                        <a href="#" class="image featured"><img src="{{asset('images/virtual/ikea-room-plan.jpg')}}" alt="" /></a>
+                    </div>
+                    <h3>Importing the model into VR</h3>
+                    <p>A designer or architect creates a 3D model of the home and The 3D model is then imported into virtual reality software, which allows the user to explore and interact with the model in a 3D environment.</p>
+                </section>
+                <section class="col-4 col-12-narrower feature">
+                    <div class="image-wrapper">
+                        <a href="#" class="image featured"><img src="{{asset('images/virtual/VR design.jpg')}}" alt="" /></a>
+                    </div>
+                    <h3>Exploring the model</h3>
+                    <p> The user can walk through the virtual home, examining details such as the layout, materials, and finishes. They can also interact with objects, such as opening doors and drawers, adjusting lighting, and moving furniture.</p>
+                </section>
+                <section class="col-4 col-12-narrower feature">
+                    <div class="image-wrapper">
+                        <a href="#" class="image featured"><img src="{{asset('images/virtual/VR-Placing-Furniture.jpg')}}" alt="" /></a>
+                    </div>
+                    <h3>Making changes</h3>
+                    <p>Based on their experience in VR, the user can make changes to the design, such as adjusting the layout or changing materials. The designer or architect can then update the 3D model to reflect these changes, and the process can be repeated until the user is satisfied with the design.</p>
+                </section>
+            </div>
 
-				
-
-				<footer class="major container medium">
-					<h3>virtual Realism</h3>
-					<p>virtual reality allows customers to better visualize and interact with their home design, providing a more immersive and engaging experience than traditional 2D drawings and renderings.</p>
-				</footer>
-
-			</div>
-
+        </section>
+    </div>
+<!-- Promo -->
+<div id="promo-wrapper">
+<section id="promo">
+<h2>virtual reality allows customers to better visualize and interact with their home design, providing a more immersive and engaging experience than traditional 2D drawings and renderings.</h2>
+</section>
+</div>
 		<!-- Scripts -->
 			<script src="{{asset('virtual_js/jquery.min.js')}}"></script>
 			<script src="{{asset('virtual_js/browser.min.js')}}"></script>
 			<script src="{{asset('virtual_js/breakpoints.min.js')}}"></script>
 			<script src="{{asset('virtual_js/util.js')}}"></script>
 			<script src="{{asset('virtual_js/main.js')}}"></script>
+			<script src="{{asset('virtual_js/jquery.dropotron.min.js')}}"></script>
 
 	</body>
 </html>
